61656bc71ecb3fb30bbbd9a59ae38bed9376f52c
sgeezy21 <26595997+sgeezy21@users.noreply.github.com>
2018-08-30 14:24:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 	// Connect to Azure SQL Database
 	$conn = ConnectToDabase();
 	// Get data for expense categories
-	$tsql="SELECT CATEGORY FROM Expense_Categories ORDER BY CATEGORY ASC";
+	$tsql="SELECT * FROM Car ORDER BY Make ASC";
 	$expenseCategories= sqlsrv_query($conn, $tsql);
 	// Populate dropdown menu options 
 	$options = '';
@@ -37,7 +37,7 @@
 	sqlsrv_close ($conn);
 	// Get the session data from the previously selected Expense Month, if it exists
 	session_start();
-	if ( !empty( $_SESSION['prevSelections'] ))
+	/*if ( !empty( $_SESSION['prevSelections'] ))
 	{ 
 		$prevSelections = $_SESSION['prevSelections'];
 		unset ( $_SESSION['prevSelections'] );
@@ -45,6 +45,7 @@
 	// Extract previously-selected Month and Year
 	$prevExpenseMonth= $prevSelections['prevExpenseMonth'];
 	$prevExpenseYear= $prevSelections['prevExpenseYear'];
+	*/
 ?>
 
 <div class="intro">
@@ -52,7 +53,7 @@
 	<h2> Input Expense Form </h2>
 
 	<!-- Display redundant error message on top of webpage if there is an error -->
-	<h3> <span class="error"> <?php echo $prevSelections['errorMessage'] ?> </span> </h3>
+	<!-- <h3> <span class="error"> <?php echo $prevSelections['errorMessage'] ?> </span> </h3> -->
 
 </div>
 
@@ -62,8 +63,7 @@ The PHP script insertToDb.php will be executed after the user clicks "Submit"-->
 <div class="container">
 	<form action="insertToDb.php" method="post">
 
-		<label>Expense Day (1-31):</label>
-		<input type="number" step="1" name="expense_day" required>
+		<label> Vehicle information :</label>
 		<input type="text" name="make" required>
 		<input type="text" name="model" required>
 		<input type="date" name="start" required>
@@ -73,15 +73,5 @@ The PHP script insertToDb.php will be executed after the user clicks "Submit"-->
 		<button type="submit">Submit</button>
 	</form>
 </div>
-
-<h3> Previous Input (if any) - for verification purposes:</h3>
-<p> Expense Day: <?php echo $prevSelections['prevExpenseDay'] ?> </p>
-<p> Expense Month: <?php echo $prevSelections['prevExpenseMonth'] ?> </p>
-<p> Expense Year: <?php echo $prevSelections['prevExpenseYear'] ?> </p>
-<p> Expense Category: <?php echo $prevSelections['prevExpenseCategory'] ?> </p>
-<p> Expense Amount: <?php echo $prevSelections['prevExpenseAmount'] ?> </p>
-<p> Expense Note: <?php echo $prevSelections['prevExpenseNote'] ?> </p>
-<p> <span class="error"> <?php echo $prevSelections['errorMessage'] ?> </span> </p>
-
 </body>
 </html>
